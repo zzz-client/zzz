19254a25d6853a6cdb6b453609fed8009e15a15c
Jonny Petraglia <jpetraglia@inspirato.com>
2023-12-30 10:50:51 -0700

    so CLEQAN

--- src/actors/client.ts
+++ src/actors/client.ts
@@ -4,30 +4,44 @@ import Letter from "../letter";
 
 export default class ClientActor implements IActor {
     async act(letter: Letter): Promise<AxiosResponse> {
-        let result;
-        if (letter.Trigger && letter.Trigger.Before) {
-            letter.Trigger.Before();
-        }
-        try {
-            console.log(`${letter.Method} ${letter.URL}`);
-            result = (
-                await axios.request({
-                    method: letter.Method,
-                    headers: letter.Headers,
-                    params: letter.QueryParams,
-                    url: letter.URL,
-                    data: letter.Body
-                })
-            ).data;
-        } catch (error) {
-            if (error.response && error.response.data) {
-                throw new Error(error.response.data);
-            }
-            throw new Error(error.code || "what");
-        }
-        if (letter.Trigger && letter.Trigger.After) {
-            letter.Trigger.After(result);
-        }
+        fireBeforeTrigger(letter);
+        const result = await fireRequest(letter);
+        fireAfterTrigger(letter, result);
         return result;
     }
+}
+function fireBeforeTrigger(letter: Letter): void {
+    if (letter.Trigger && letter.Trigger.Before) {
+        letter.Trigger.Before();
+    }
+}
+function fireAfterTrigger(letter: Letter, data: any): void {
+    if (letter.Trigger && letter.Trigger.After) {
+        letter.Trigger.After(data);
+    }
+}
+function formatError(error: any) {
+    if (error.response && error.response.data) {
+        return error.response.data;
+    }
+    return error.code;
+}
+async function fireRequest(letter: Letter): Promise<any> {
+    try {
+        console.log(`${letter.Method} ${letter.URL}`);
+        return await doRequest(letter);
+    } catch (error) {
+        throw new Error(formatError(error));
+    }
+}
+async function doRequest(letter: Letter): Promise<any> {
+    return (
+        await axios.request({
+            method: letter.Method,
+            headers: letter.Headers,
+            params: letter.QueryParams,
+            url: letter.URL,
+            data: letter.Body
+        })
+    ).data;
 }
