eac19c5fae606b75c0c598a97ec4a8fdb207d6c0
Jonny Petraglia <jpetraglia@inspirato.com>
2024-01-02 10:02:25 -0700

    do

--- src/web.js
+++ src/web.js
@@ -5,13 +5,11 @@ import ToastService from "primevue/toastservice";
 import Toast from "primevue/toast";
 import Badge from "primevue/badge";
 import BadgeDirective from "primevue/badgedirective";
-import axios from "https://deno.land/x/redaxios/mod.ts";
-import VueAxios from "vue-axios";
-Vue.use(VueAxios, axios);
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.use(ToastService);
+
 app.component("Toast", Toast);
 app.component("Toast", Badge);
 app.directive("badge", BadgeDirective);
