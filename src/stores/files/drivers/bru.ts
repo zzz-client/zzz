43363d8f21aa3515dfa915df346009b81a6cc274
jonny@beingpresent.me <jonny@beingpresent.me>
2024-01-15 02:00:40 -0700

    Moved stores

---
+++ src/stores/files/drivers/bru.ts
@@ -0,0 +1,117 @@
+import { Driver } from "../drivers.ts";
+
+const BRU = {
+  parse,
+  stringify,
+} as Driver;
+export default BRU;
+
+// TODO: Replace with @usebruno/lang to get all the goodies
+function parse(input: string): any {
+  let result = {};
+  const lines = input.split("\n");
+  let index = 0;
+  while (index < lines.length) {
+    const [block, newIndex] = readBlock(lines, index);
+    result = { ...result, ...parseBlock(block) };
+    index = newIndex;
+  }
+  return result;
+}
+function stringify(input: any): string {
+  const results = [] as string[];
+  results.push("meta {");
+  results.push(`  name: ${input.Name}`);
+  results.push(`  type: http`);
+  // TODO: Sequence
+  results.push("}");
+  if (input.Method) {
+    results.push(
+      `${input.Method.toLowerCase()} {`,
+      `  url: ${input.URL}`,
+      `}`,
+    );
+  }
+  if (input.Headers) {
+    results.push("headers {");
+    for (const header in input.Headers) {
+      results.push(`  ${header}: ${input.Headers[header]}`);
+    }
+    results.push("}");
+  }
+  if (input.QueryParams) {
+    results.push("query {");
+    for (const param in input.QueryParams) {
+      results.push(`  ${param}: ${input.QueryParams[param]}`);
+    }
+    results.push("}");
+  }
+  if (input.Body) {
+    results.push("body {");
+    // TODO: body, body:text, body:xml, body:form-urlencoded, body:multipart-form
+    results.push("}");
+  }
+  // TODO: script:post-response & script:pre-request (javascript), tests
+  return results.join("\n");
+}
+
+function readBlock(lines: string[], index: number): [any, number] {
+  while (!lines[index].endsWith("{") || lines[index].trim() === "") {
+    index++;
+  }
+  const tag = lines[index].substring(0, lines[index].length - 2);
+  index++; // Skip the opening brace
+  const block = { tag: tag };
+  while (!lines[index].startsWith("}")) {
+    const [key, value] = lines[index].split(": ");
+    block[key.trim()] = value;
+    index++;
+  }
+  index++; // Skip the closing brace
+  return [block, index];
+}
+function parseBlock(block: any): any {
+  const parseMethod = blockParsers[block.tag];
+  if (!parseMethod) {
+    throw new Error(`Unsupported Bru tag: '${block.tag}'`);
+  }
+  return parseMethod(block);
+}
+function parseMetaBlock(block: any): any {
+  return {
+    Name: block.name,
+  };
+}
+function parseMethodBlock(block: any): any {
+  return {
+    Method: block.tag.toUpperCase(),
+    URL: block.url,
+  };
+}
+function parseMap(block: any): any {
+  const body = {};
+  for (const key in block) {
+    if (key !== "tag") {
+      body[key] = block[key];
+    }
+  }
+  const result = {};
+  result[block.tag == "query" ? "QueryParams" : block.tag.toUpperCase()] = body;
+  return result;
+}
+const blockParsers = {
+  meta: parseMetaBlock,
+  get: parseMethodBlock,
+  post: parseMethodBlock,
+  put: parseMethodBlock,
+  patch: parseMethodBlock,
+  delete: parseMethodBlock,
+  options: parseMethodBlock,
+  query: parseMap,
+  headers: parseMap,
+  body: parseBody,
+};
+function parseBody(block: any): any {
+  // TODO
+  return parseMap(block);
+}
---
+++ src/stores/files/index.ts
@@ -0,0 +1,100 @@
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
+import { Collection, Entity, Model, ModelType, StringToStringMap } from "../../core/models.ts";
+import { basename, extname } from "https://deno.land/std/path/mod.ts";
+import { IStore } from "../../core/app.ts";
+import { Driver, getDriver } from "../../stores/files/drivers.ts";
+
+const SESSION_FILE = "session.local";
+
+export default class FileStore implements IStore {
+  fileExtension: string;
+  constructor(fileExtension: string) {
+    this.fileExtension = fileExtension;
+  }
+  async get(modelType: ModelType, entityId: string, context: string): Promise<Model> {
+    if (modelType === ModelType.Entity) {
+      const requestPath = entityId + "." + this.fileExtension;
+      const resultRequest = await (await getDriver(requestPath)).parse(Deno.readTextFileSync(requestPath)) as Entity;
+      resultRequest.Id = entityId;
+      resultRequest.Type = ModelType[modelType];
+      if (!resultRequest.Name) {
+        resultRequest.Name = basename(entityId);
+      }
+      return resultRequest;
+    } else if (modelType === ModelType.Collection) {
+      const item = new (modelType === ModelType.Collection ? Collection : Collection)(entityId, basename(entityId));
+      for await (const child of Deno.readDir(entityId)) {
+        if (child.isDirectory) {
+          item.Children.push(await this.get(ModelType.Collection, `${entityId}/${child.name}`, context));
+        } else if (child.isFile && filetypeSupported(child.name) && !excludeFromInfo(child.name)) {
+          const baseless = basename(child.name, extname(child.name));
+          item.Children.push(await this.get(ModelType.Entity, `${entityId}/${baseless}`, context));
+        }
+      }
+      item.Id = entityId;
+      item.Type = ModelType[modelType];
+      return item;
+    }
+    if (modelType === ModelType.Context || modelType === ModelType.Authorization) {
+      const entityFolder = getDirectoryForModel(modelType);
+      const filePath = `${entityFolder}/${entityId}.${this.fileExtension}`;
+      console.log("Filepath: ", filePath);
+      const item = this._driver().parse(Deno.readTextFileSync(filePath)) as Model; // TODO: Is this a naughty cast?
+      item.Id = entityId;
+      item.Type = ModelType[modelType];
+      return item;
+    }
+    throw new Error(`Unknown type of entity: ${modelType}`);
+  }
+  store(key: string, value: any): Promise<void> {
+    const sessionPath = getDirectoryForModel(ModelType.Context) + "/" + SESSION_FILE + "." + this.fileExtension;
+    let sessionContents = { Variables: {} as StringToStringMap };
+    if (existsSync(sessionPath)) {
+      sessionContents = this._driver().parse(Deno.readTextFileSync(sessionPath));
+    }
+    sessionContents.Variables[key] = value;
+    Deno.writeTextFileSync(sessionPath, this._driver().stringify(sessionContents));
+    return Promise.resolve();
+  }
+  setContext(context: string): void {
+    throw new Error("Not implemented");
+  }
+  _driver(): Driver {
+    return getDriver("." + this.fileExtension);
+  }
+}
+
+function filetypeSupported(filePath: string): boolean {
+  try {
+    getDriver(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+function excludeFromInfo(name: string): boolean {
+  return name.startsWith("_");
+}
+// async function determineType(modelId: string): Promise<ModelType> {
+//   if (modelId.startsWith(getDirectoryForModel(ModelType.Authorization))) {
+//     return ModelType.Authorization;
+//   }
+//   if (modelId.startsWith(getDirectoryForModel(ModelType.Context))) {
+//     return ModelType.Context;
+//   }
+//   if (existsSync(modelId, { isFile: true })) {
+//     return ModelType.Entity;
+//   } else if (modelId.includes("/")) {
+//     return ModelType.Collection;
+//   }
+// }
+export function getDirectoryForModel(modelType: ModelType): string {
+  switch (modelType) {
+    case ModelType.Context:
+      return "contexts";
+    case ModelType.Authorization:
+      return "authorizations";
+    default:
+      throw new Error(`Unknown entity type ${ModelType[modelType]}`);
+  }
+}
