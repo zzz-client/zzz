aae0b4dd6f9a7ad837b3f5ffed1b3b9b46410387
Jonny Petraglia <jpetraglia@inspirato.com>
2024-01-23 15:51:40 -0700

    Final auth fixes

--- src/apps/zzz/modules/auth/mod.ts
+++ src/apps/zzz/modules/auth/mod.ts
@@ -6,12 +6,14 @@ import HeaderAuthorizer from "./header.ts";
 import { QueryAuthorizer } from "./query.ts";
 import { Action } from "../../../../lib/lib.ts";
 import { Model } from "../../../../storage/mod.ts";
+import { ContextModule } from "../context/mod.ts";
 
 export class AuthorizationModule extends Module implements IModuleModels, IModuleFields, IModuleModifier {
-  dependencies = [RequestsModule.constructor.name];
+  dependencies = [RequestsModule.constructor.name, ContextModule.constructor.name];
   models = [Authorization.constructor.name];
   fields = {
     Authorization: Authorization,
+    HttpRequest: ChildAuthorization,
   };
   async modify(model: Model, action: Action): Promise<void> {
     if (AuthorizationModule.hasFields(model) && model instanceof HttpRequest) {
@@ -23,11 +25,11 @@ export class AuthorizationModule extends Module implements IModuleModels, IModul
       if (typeof auth === "string") {
         auth = await this.app.store.get(Authorization.name, auth) as Authorization; // TODO Why is this a never????
       }
-      // if (action.features.all) {
-      (model as any).Authorization = auth;
-      // }
-      if (action.features.all || action.features.execute) {
-        const authType = "BearerToken"; // TODO: Somehow get root key?
+      if (action.features.all) {
+        (model as any).Authorization = auth;
+      }
+      if (action.features.execute) { // TODO: Should this be for format too? if so does this module depend on template? that seems backwards??? Maybe dependencies should only be those that are required, but then how does this class state that it knows about Template?
+        const authType = Object.keys(auth)[0];
         console.log(auth);
         auth = auth[authType] as Authorization;
         const authorizer = this.newAuthorization(authType);
@@ -58,4 +60,7 @@ export type AuthContents = {};
 
 export class Authorization extends Model {
   [key: string]: AuthContents | Authorization;
+}
+class ChildAuthorization {
+  Authorization?: Authorization | string;
 }
