c5f7613d919fa766d73ad51351c04c5565e2ddf9
Jonny Petraglia <jpetraglia@inspirato.com>
2024-02-10 19:38:36 -0700

    bro tests

--- src/apps/zzz/modules/auth/mod.ts
+++ src/apps/zzz/modules/auth/mod.ts
@@ -14,39 +14,44 @@ export class AuthorizationModule extends Module implements IModuleModels, IModul
     HttpRequest: ChildAuthorization,
   };
   async modify(model: Model, action: Action): Promise<void> {
-    Trace("AuthorizationModule:modify");
-    if (AuthorizationModule.hasFields(model)) {
-      Trace("Has Authorization");
-      let auth = asAny(model).Authorization as Authorization;
-      if (!auth) {
-        Trace("Authorization is undefined, aborting");
-        return Promise.resolve();
-      }
-      if (typeof auth === "string") {
-        Trace("Looking up Authorization profile:", auth);
-        auth = await this.store.get(Authorization.name, auth) as Authorization;
-      }
-      if (typeof auth === "string") {
-        Trace("Loading stored Authorization:", auth);
-        auth = await this.store.get(Authorization.name, auth) as Authorization;
-      }
-      Trace("Authorization:", auth);
-      if (action.features.all) {
-        Trace("Setting Authorization attribute on Model");
-        asAny(model).Authorization = auth;
-      }
-      if (action.features.execute) { // TODO: Should this be for format too? if so does this module depend on template? that seems backwards??? Maybe dependencies should only be those that are required, but then how does this class state that it knows about Template?
-        Trace("Executing");
-        const authType = Object.keys(auth)[0];
-        Trace("AuthType:", authType);
-        auth = auth[authType] as Authorization;
-        Trace("Authorization:", Authorization);
-        const authorizer = DI.newInstance("IAuthorizer", authType) as IAuthorizer;
-        Trace("Authorizer", authorizer);
-        return authorizer.authorize(model, auth);
-      }
+    // Trace("AuthorizationModule:modify");
+    if (!AuthorizationModule.hasFields(model)) {
+      return;
+    }
+
+    // Trace("Has Authorization");
+    let auth = asAny(model).Authorization as Authorization;
+    if (!auth) {
+      // Trace("Authorization is undefined, aborting");
       return Promise.resolve();
     }
+
+    if (typeof auth === "string") {
+      // Trace("Loading stored Authorization:", auth);
+      auth = await this.store.get(Authorization.name, auth) as Authorization;
+    }
+
+    // Trace("Authorization:", auth);
+    if (action.features.all) {
+      // Trace("Setting Authorization attribute on Model");
+      asAny(model).Authorization = auth;
+    }
+
+    if (action.features.execute) { // TODO: Should this be for format too? if so does this module depend on template? that seems backwards??? Maybe dependencies should only be those that are required, but then how does this class state that it knows about Template?
+      this.execute(model, auth);
+    }
+    return Promise.resolve();
+  }
+  // deno-lint-ignore no-explicit-any
+  private execute(model: Model, auth: any): void {
+    // Trace("Executing");
+    const authType = Object.keys(auth)[0];
+    // Trace("AuthType:", authType);
+    auth = auth[authType] as Authorization;
+    // Trace("Authorization:", Authorization);
+    const authorizer = DI.newInstance("IAuthorizer", authType) as IAuthorizer;
+    // Trace("Authorizer", authorizer);
+    authorizer.authorize(model, auth);
   }
 }
 export interface IAuthorizer {
@@ -59,6 +64,7 @@ export class Authorization extends Model {
   [key: string]: AuthContents | Authorization;
 }
 class ChildAuthorization {
+  // deno-coverage-ignore
   Authorization?: Authorization | string;
 }
 
@@ -93,6 +99,20 @@ describe("Authorization Module ", () => {
         args: ["Authorization", "asdf"],
         returned: Promise.resolve(testRequest),
       });
+      getStub.restore();
+    });
+    it("does nothing when .Authorization is already complex object", async () => {
+      const testStore = new TestStore();
+      const testRequest = new HttpRequest();
+      const getStub = stub(testStore, "get", resolvesNext([testRequest]));
+      // GIVEN
+      const module = new AuthorizationModule(testStore);
+      const action = new Action({}, {});
+      asAny(testRequest).Authorization = { BearerToken: "asdf" };
+      // WHEN
+      await module.modify(testRequest, action);
+      // THEN
+      assertEquals(asAny(testRequest).Authorization, { BearerToken: "asdf" }, "Authorization should be string it was set to");
       getStub.restore();
     });
   });
