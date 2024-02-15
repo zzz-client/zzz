10f60d3b2895647d0c84f65abaf1f3a45343779e
Jonny Petraglia <jpetraglia@inspirato.com>
2024-02-14 20:25:55 -0700

    yay test pass

--- src/lib/tests.ts
+++ src/lib/tests.ts
@@ -5,7 +5,7 @@ import { IStore, Model } from "../storage/mod.ts";
 export { describe, it } from "https://deno.land/std/testing/bdd.ts";
 export { fail } from "https://deno.land/std/assert/fail.ts";
 export { assertEquals } from "https://deno.land/std/assert/mod.ts";
-export { assertSpyCall, resolvesNext, stub } from "https://deno.land/x/mock/mod.ts";
+export { assertSpyCall, resolvesNext, returnsNext, stub } from "https://deno.land/x/mock/mod.ts";
 
 export class TestStore implements IStore {
   get(modelType: string, id: string): Promise<Model> {
