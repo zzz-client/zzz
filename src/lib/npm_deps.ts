3522ffc2cf15671536c836a2f7df37afd6f14345
Jonny Petraglia <jpetraglia@inspirato.com>
2024-02-14 23:17:57 -0700

    primevue issue

--- src/lib/npm_deps.ts
+++ src/lib/npm_deps.ts
@@ -3,3 +3,4 @@ export { RadioButton } from "npm:primevue/radiobutton";
 export { DropDown } from "npm:primevue/dropdown";
 export type { MenuItem } from "npm:primevue/menuitem";
 export { TabView } from "npm:primevue/tabview";
+export { Config } from "npm:primevue/config";
