e72e646e962627d3a29ce8c0c323ad6cb653fd8c
Jonny Petraglia <jpetraglia@inspirato.com>
2024-02-14 20:21:36 -0700

    Body Module tests, enough for now

--- src/apps/zzz/modules/body/mod.ts
+++ src/apps/zzz/modules/body/mod.ts
@@ -12,20 +12,18 @@ export class BodyModule extends Module implements IModuleModifier, IModuleFields
   async modify(entity: Model, _action: Action): Promise<void> {
     Trace("BodyModule:modify");
     if (Module.hasFields(entity, ["Body"]) && entity instanceof HttpRequest) {
-      await this.loadBody(entity);
+      await loadBody(entity);
     }
     return Promise.resolve();
   }
-  private loadBody(entity: Model): Promise<void> {
-    let body = asAny(entity).Body;
-    if (typeof body === "string") {
-      body = Deno.readTextFileSync(body);
-    }
-    if (body) {
-      asAny(entity).Body = body;
-    }
-    return Promise.resolve();
+}
+
+function loadBody(entity: Model): Promise<void> {
+  const body = asAny(entity).Body;
+  if (typeof body === "string") {
+    asAny(entity).Body = Deno.readTextFileSync(body);
   }
+  return Promise.resolve();
 }
 
 class BodyFields {
@@ -34,10 +32,40 @@ class BodyFields {
 
 // ----------------------------------------- TESTS -----------------------------------------
 
-import { describe, fail, it } from "../../../../lib/tests.ts";
+import { assertEquals, assertSpyCall, describe, it, returnsNext, stub, TestStore } from "../../../../lib/tests.ts";
+import { returnsArg } from "https://deno.land/x/mock@0.15.2/mod.ts";
 
-describe("Bearer Token Authorizer", () => {
-  it("sets header correctly", async () => {
-    // fail("Write this test");
+describe("Body Module", () => {
+  it("sets object body correctly", async () => {
+    const testStore = new TestStore();
+    const module = new BodyModule(testStore);
+    // GIVEN
+    const request = new HttpRequest();
+    asAny(request).Body = { foo: "bar" };
+    const action = new Action({}, {});
+    // WHEN
+    module.modify(request, action);
+    // THEN
+    assertEquals(asAny(request).Body, { foo: "bar" }, "Body attribute not set correctly");
+  });
+  it("sets file body correctly", async () => {
+    const testStore = new TestStore();
+    const module = new BodyModule(testStore);
+    const testBodyContents = '{"foo":"bar"}';
+    const readTextFileStub = stub(Deno, "readTextFileSync", returnsNext([testBodyContents]));
+    // GIVEN
+    const request = new HttpRequest();
+    asAny(request).Body = "test/file/path";
+    const action = new Action({}, {});
+    // WHEN
+    module.modify(request, action);
+    console.log("BODY", asAny(request).Body);
+    // THEN
+    assertSpyCall(readTextFileStub, 0, {
+      args: ["test/file/path"],
+      returned: testBodyContents,
+    });
+    assertEquals(asAny(request).Body, testBodyContents, "Body attribute not set correctly");
+    readTextFileStub.restore();
   });
 });
