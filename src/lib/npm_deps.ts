c7582a3c10471db5125b4fe6377e77248e7ecbd6
Jonny Petraglia <jpetraglia@inspirato.com>
2024-02-14 23:15:42 -0700

    about ready to give up on building web for now

---
+++ src/lib/npm_deps.ts
@@ -0,0 +1,5 @@
+export { DataTable } from "npm:primevue/datatable";
+export { RadioButton } from "npm:primevue/radiobutton";
+export { DropDown } from "npm:primevue/dropdown";
+export type { MenuItem } from "npm:primevue/menuitem";
+export { TabView } from "npm:primevue/tabview";
