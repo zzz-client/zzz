dd2c409ea750604b88849cc4f4fb9c6994f05139
Jonny Petraglia <jpetraglia@inspirato.com>
2024-03-05 22:06:56 -0700

    Hey look mild tui

--- src/apps/zzz/interfaces/tui/tui.ts
+++ src/apps/zzz/interfaces/tui/tui.ts
@@ -1,41 +1,59 @@
 // deno-lint-ignore-file
 import { crayon } from "https://deno.land/x/crayon/mod.ts";
-import { Canvas, Tui } from "https://deno.land/x/tui/mod.ts";
+import { Canvas, Tui as TuiMod } from "https://deno.land/x/tui/mod.ts";
 import { handleInput, handleKeyboardControls, handleMouseControls } from "https://deno.land/x/tui/mod.ts";
 import { Button } from "https://deno.land/x/tui/src/components/mod.ts";
 import { Computed, Signal } from "https://deno.land/x/tui/mod.ts";
-import { Table } from "https://deno.land/x/tui/src/components/table.ts";
+import { Table, TableOptions } from "https://deno.land/x/tui/src/components/table.ts";
+import IApplication from "../../../mod.ts";
+import { Collection } from "../../modules/requests/mod.ts";
+import { Model } from "../../../../storage/mod.ts";
+import { Scope } from "../../modules/scope/mod.ts";
 
-// const tui = new Tui({
-//   style: crayon.bgBlack, // Make background black
-//   refreshRate: 1000 / 60, // Run in 60FPS
-// });
+export default async function Tui(app: IApplication): Promise<void> {
+  const tui = new TuiMod({
+    style: crayon.bgBlack, // Make background black
+    refreshRate: 1000 / 60, // Run in 60FPS
+  });
+  tui.dispatch(); // Close Tui on CTRL+C
+  handleInput(tui);
+  handleMouseControls(tui);
+  handleKeyboardControls(tui);
 
-// tui.dispatch(); // Close Tui on CTRL+C
+  const collections = {
+    parent: tui,
+    theme: {
+      base: crayon.bgBlack.white,
+      frame: { base: crayon.bgBlack },
+      header: { base: crayon.bgBlack.bold.lightBlue },
+      selectedRow: {
+        base: crayon.bold.bgBlue.white,
+        focused: crayon.bold.bgLightBlue.white,
+        active: crayon.bold.bgMagenta.black,
+      },
+    },
+    rectangle: {
+      column: 1,
+      row: 1,
+      height: 10,
+    },
+    headers: [
+      // { title: "ID" },
+      { title: "Name" },
+    ],
+    data: [],
+    charMap: "rounded",
+    zIndex: 0,
+  } as TableOptions;
 
-// handleInput(tui);
-// handleMouseControls(tui);
-// handleKeyboardControls(tui);
+  (await app.store.get(Scope.name, "Salesforce Primary")).Children.forEach((collection: Model) => {
+    collections.data.push([
+      // collection.Id,
+      collection.Name,
+    ]);
+  });
+  const collectionsWidget = new Table(collections);
 
-// const what = [
-//   ["foo", "bar"],
-// ];
-
-// const collections = new Table({
-//   parent: tui,
-//   headers: { title: [] },
-//   data: what,
-//   theme: {
-//     header: { base: () => "X" },
-//     base: crayon.bgRed,
-//     focused: crayon.bgLightRed,
-//     active: crayon.bgYellow,
-//   },
-//   rectangle: {
-//     column: 1,
-//     row: 1,
-//     height: 5,
-//   },
-// });
-
-// tui.run();
+  tui.run();
+  return Promise.resolve();
+}
---
+++ src/tui.ts
@@ -0,0 +1,7 @@
+import { initDi } from "./apps/zzz/app.ts";
+import Application from "./apps/zzz/interfaces/tui/app.ts";
+
+if ((import.meta as any).main) {
+  initDi();
+  await new Application().run();
+}
