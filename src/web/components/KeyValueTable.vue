6de572a284e3366ace29fdef90279dfd4006f83a
Jonny Petraglia <jpetraglia@inspirato.com>
2024-03-19 00:20:48 -0600

    KeyValueTable is mostly working, somehow! Closes #21. Close #21

--- src/web/components/KeyValueTable.vue
+++ src/web/components/KeyValueTable.vue
@@ -2,7 +2,7 @@
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
 import InputText from "primevue/inputtext";
-import { ref, toRef, watch } from "vue";
+import { ref, toRef, watch, reactive } from "vue";
 
 interface KeyValue {
   key: string;
@@ -17,41 +17,51 @@ const model = defineModel({ type: Object, default: {} });
 
 const what = ref([]);
 
-watch(
-  () => model.value,
-  (newModel) => {
-    const modelWhat = JSON.parse(JSON.stringify(model.value));
-    console.log("Model", modelWhat);
-    what.value = [];
-    for (const key in modelWhat) {
-      console.log("Key", key);
-      what.value.push({
-        key: key,
-        value: model.value[key]
-      });
-    }
+watch(model, (newModel) => {
+  const modelWhat = JSON.parse(JSON.stringify(model.value));
+  console.log("KeyValue", modelWhat);
+  what.value = [];
+  for (const key in modelWhat) {
+    console.log("Key", key, "Value", model.value[key]);
+    what.value.push({
+      key: key,
+      value: model.value[key]
+    });
   }
-);
+});
+function onKeyChange(event) {
+  console.log(event);
+  const oldKey = event.target.placeholder;
+  const newKey = event.target.value;
+  const index = what.value.findIndex((item) => {
+    console.log(item.key, "vs", oldKey);
+    return item.key === oldKey;
+  });
+  if (index === -1) {
+    alert("No!");
+    return;
+  }
+  console.log("what", index, what.value[index]);
+  const oldValue = what.value[index].value;
+  delete model.value[oldKey];
+  model.value[newKey] = oldValue;
+  what.value[index].key = newKey;
+  event.target.placeholder = newKey;
+}
 </script>
 
 <template>
   <div class="uwu">
-    Model {{ model }}
-    <br />
-    What {{ what }}
-    <DataTable v-model:data="what" :editMode="props.readOnly ? 'row' : 'cell'">
-      <!-- editMode="cell"-->
-      <Column key="key" field="key" header="Key">
-        <template #editor="{ what, field }">
-          Hello!
-          <InputText v-model="model[field]" autofocus placeholder="Key" />
+    <DataTable :value="what" :editMode="props.readOnly ? 'row' : 'cell'">
+      <Column key="Key" field="key" header="Key">
+        <template #editor="{ data, field }">
+          <InputText @input="onKeyChange" :value="data.key" autofocus :placeholder="data.key || 'Key'" />
         </template>
       </Column>
       <Column key="value" field="value" header="Value" style="width: 75%">
-        {{ value }}
-        <!-- <template #editor="{ what, field }">
-          <InputText v-model="model[field]" autofocus placeholder="Value" />
-        </template> -->
+        <template #editor="{ data, field }">
+          <InputText v-model="model[data.key]" autofocus :placeholder="model[data.key] || 'Value'" />
+        </template>
       </Column>
     </DataTable>
   </div>
