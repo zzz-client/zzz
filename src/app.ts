749f0a82fd12f2eff0e268ac491b760ba90d5be2
Jonny Petraglia <jpetraglia@inspirato.com>
2024-03-20 18:30:37 -0600

    Reworked Scopes! for the file store, anyway. Now they are stored in a single YML

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import HeaderAuthorizer from "./core/modules/auth/header.ts";
 import FileStorage from "./core/storage/files/mod.ts";
 import QueryAuthorizer from "./core/modules/auth/query.ts";
 import FileStore from "./core/stores/files.ts";
+import ConfigStorage from "./core/storage/config/mod.ts";
 
 // deno-fmt-ignore
 export function initDi(): void{
@@ -13,8 +14,8 @@ export function initDi(): void{
   DI.register("IAuthorizer",            () => new HeaderAuthorizer());
   DI.register("IAuthorizer",            () => new QueryAuthorizer());
   DI.register("IStore",                 () => new FileStore());
+  DI.register("IStorage:Scope",         () => new ConfigStorage('zzz.yml'));
   DI.register("IStorage:HttpRequest",   () => new FileStorage('library', 'yml'));
-  DI.register("IStorage:Scope",         () => new FileStorage('library', 'yml'));
   DI.register("IStorage:Collection",    () => new FileStorage('library', 'yml'));
   DI.register("IStorage:Context",       () => new FileStorage('library/contexts', 'yml'));
   DI.register("IStorage:Authorization", () => new FileStorage('library/auth', 'yml'));
