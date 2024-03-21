1d3f2dd34e162988107a42404e1f35dab806e172
Jonny Petraglia <jpetraglia@inspirato.com>
2024-03-21 03:32:58 -0600

    An untested PKGBUILD

--- scripts/setVersion.ts
+++ scripts/setVersion.ts
@@ -7,3 +7,4 @@ Deno.writeTextFileSync('deno.jsonc', Deno.readTextFileSync('deno.jsonc').replace
 Deno.writeTextFileSync('src/desktop/tauri.conf.json', Deno.readTextFileSync('src/desktop/tauri.conf.json').replace(/"version": ".+"/, `"version": "${newVersion}"`))
 Deno.writeTextFileSync('src/desktop/Cargo.toml', Deno.readTextFileSync('src/desktop/Cargo.toml').replace(/version = ".+"/, `version = "${newVersion}"`))
 Deno.writeTextFileSync('scripts/desktop.nsi', Deno.readTextFileSync('scripts/desktop.nsi').replace(/\!define VERSION ".+"/, `!define VERSION "${newVersion}"`))
+Deno.writeTextFileSync('scripts/cli.PKGBUILD', Deno.readTextFileSync('scripts/desktop.nsi').replace(/^pkgver=.+$/, `pkgver=${newVersion}`))
