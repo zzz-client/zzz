0754a299c4924405678cadb36126426aefcdcaa3
Jonny Petraglia <jpetraglia@inspirato.com>
2024-03-21 02:45:17 -0600

    NSIS installer!

--- scripts/setVersion.ts
+++ scripts/setVersion.ts
@@ -6,3 +6,4 @@ if (!newVersion) {
 Deno.writeTextFileSync('deno.jsonc', Deno.readTextFileSync('deno.jsonc').replace(/"version": ".+"/, `"version": "${newVersion}"`))
 Deno.writeTextFileSync('src/desktop/tauri.conf.json', Deno.readTextFileSync('src/desktop/tauri.conf.json').replace(/"version": ".+"/, `"version": "${newVersion}"`))
 Deno.writeTextFileSync('src/desktop/Cargo.toml', Deno.readTextFileSync('src/desktop/Cargo.toml').replace(/version = ".+"/, `version = "${newVersion}"`))
+Deno.writeTextFileSync('scripts/desktop.nsi', Deno.readTextFileSync('scripts/desktop.nsi').replace(/\!define VERSION ".+"/, `!define VERSION "${newVersion}"`))
